7a87d00617f120e6e15724acf7e3dccbc8caf913
soumya-metagame <soumya@meta-game.ai>
2024-02-01 22:29:46 +0530

    url updates

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const Config = {
-    BaseUrl : 'http://15.207.108.234:4000/api/v1',
-    sockerUrl:'http://15.207.108.234:4000'
+    BaseUrl : 'https://deposit.skillzlive.in/api/v1',
+    sockerUrl:'https://deposit.skillzlive.in'
 }
