73fd3630bf4c1fef0e6726347c49091fc6979037
soumya-metagame <soumya@meta-game.ai>
2024-02-01 21:10:40 +0530

    url update

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const Config = {
-    BaseUrl : 'http://3.109.170.160:4000/api/v1',
-    sockerUrl:'http://3.109.170.160:4000'
+    BaseUrl : 'http://15.207.108.234:4000/api/v1',
+    sockerUrl:'http://15.207.108.234:4000'
 }
