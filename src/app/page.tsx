98ea088f0818f939e126217ed42aaa5bdb90a893
soumya-metagame <soumya@meta-game.ai>
2024-02-06 15:56:40 +0530

    suspence added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import StoreRubiesCard from "@/components/store-rubies-card";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-[#00001C]">
+      <Suspense fallback={<><h1>Loading...</h1></>}>
+
       <StoreRubiesCard />
+      </Suspense>
     </main>
   );
 }
