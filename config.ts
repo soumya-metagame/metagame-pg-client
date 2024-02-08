ee1ed1b33ce741b02ba56b9435e0e319954bf6c2
Dwitidibyajyoti-meta-ai <dwitidibyajyoti@meta-game.ai>
2024-02-08 17:07:20 +0530

    update server url

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const Config = {
     BaseUrl : 'https://deposit.skillzlive.in/server/api/v1',
-    UrlForUpdate : 'https://deposit.skillzlive.in/api/balance',
+    UrlForUpdate : 'https://deposit.skillzlive.in/server/api/balance',
     sockerUrl:'https://deposit.skillzlive.in/'
 }
